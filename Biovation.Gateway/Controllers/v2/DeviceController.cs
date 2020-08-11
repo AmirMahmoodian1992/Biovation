3f8d9ee59c64a57bf26dc6d35af1e111b72838d7
arash <sarhadiarash@yahoo.com>
2020-08-11 11:45:55 +0430

    [Gateway]: Create the structure of V2 API for Device controller

---
+++ Biovation.Gateway/Controllers/v2/DeviceController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Biovation.CommonClasses;
+using Biovation.CommonClasses.Manager;
+using Biovation.CommonClasses.Models;
+using Biovation.CommonClasses.Service;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Biovation.Gateway.Controllers.v2
+{
+    //[Route("Biovation/Api/{controller}/{action}", Name = "Device")]
+    [Route("[controller]")]
+    public class DeviceController : Controller
+    {
+        private readonly DeviceService _deviceService = new DeviceService();
+        private readonly UserService _userService = new UserService();
+        private readonly RestClient _restClient;
+
+        
+        public DeviceController()
+        {
+            _restClient = (RestClient)new RestClient($"http://localhost:{BiovationConfigurationManager.BiovationWebServerPort}/Biovation/Api/").UseSerializer(() => new RestRequestJsonSerializer());
+        }
+
+
+        //TODO compatible with paging(az yaser bepors)
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<JsonResult> Devices(long id =0, long adminUserId = 0, int deviceGroupId = 0, uint code = 0, 
+            int deviceId = 0, int brandId = 0, string deviceName = null, int deviceModelId = 0, int deviceTypeId = 0, 
+            string brandCode = null, bool loadedBrandsOnly = true)
+        {
+            throw null;
+        }
+
+        [HttpPost]
+        public Task<JsonResult> DeviceInfo([FromBody]DeviceBasicInfo device)
+        {
+            throw null;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Task<JsonResult> DeleteDevice(uint id)
+        {
+            throw null;
+        }
+
+        [HttpGet]
+        [Route("OfflineLog")]
+        public Task<JsonResult> OfflineLogs(string deviceIds, string fromDate, string toDate)
+        {
+            throw null;
+        }
+
+
+        [HttpPost]
+        [Route("DeleteDevices")]
+        public Task<JsonResult> DeleteDevices([FromBody]List<uint> ids)
+        {
+            throw null;
+        }
+
+        [HttpGet]
+        [Route("OnlineDevices")]
+        public Task<JsonResult> OnlineDevices()
+        {
+            throw null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deviceId"></param>
+        /// <param name="userId">Json list of userIds</param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UserFromDevice")]
+        public async Task<IActionResult> UserDevice(int deviceId, [FromBody]JArray userId)
+        {
+            //return NotFound();
+            throw null;
+        }
+
+        [HttpPut]
+        [Route("UsersListFromDevice")]
+        public IActionResult UsersOfDevice(int deviceId)
+        {
+            throw null;
+        }
+
+        [HttpPost]
+        [Route("DeleteUserFromDevice")]
+        public Task<JsonResult> UsersFromDevice(int deviceId, [FromBody]JArray userId)
+        {
+            throw null;
+        }
+
+        [HttpPut]
+        [Route("SendUsersToDevice")]
+        public Task<JsonResult> SendUsersToDevice(int deviceId)
+        {
+            throw null;
+        }
+
+        [HttpGet]
+        [Route("DeviceInfo")]
+        public Task<JsonResult> DeviceInfo(int deviceId)
+        {
+            throw null;
+        }
+
+
+        //TODO check it wtf?
+        [HttpPost]
+        [Route("DevicesDataToDevice")]
+        public Task<JsonResult> DevicesDataToDevice([FromBody]List<int> deviceIds, int deviceId = default)
+        {
+            throw null;
+        }
+            
+
+        //TODO make compatible with .net core
+        //[HttpPost]
+        //[Route("UpgradeFirmware")]
+        //public Task<ResultViewModel> UpgradeFirmware(int deviceId)
+        //{
+        //    return Task.Run(async () =>
+        //    {
+        //        if (!Request.Content.IsMimeMultipartContent())
+        //            return new ResultViewModel { Validate = 0, Code = 415, Message = "UnsupportedMediaType" };
+
+        //        try
+        //        {
+        //            var device = _deviceService.GetDeviceInfo(deviceId);
+
+        //            if (device is null)
+        //                return new ResultViewModel
+        //                { Validate = 0, Code = 400, Id = deviceId, Message = "Wrong device id provided" };
+
+        //            var multipartMemory = await Request.Content.ReadAsMultipartAsync();
+
+        //            foreach (var multipartContent in multipartMemory.Contents)
+        //            {
+        //                try
+        //                {
+        //                    var restRequest = new RestRequest($"{device.Brand.Name}/{device.Brand.Name}Device/UpgradeFirmware", Method.POST, DataFormat.Json);
+        //                    restRequest.AddHeader("Content-Type", "multipart/form-data");
+        //                    restRequest.AddQueryParameter("deviceCode", device.Code.ToString());
+        //                    restRequest.AddFile(multipartContent.Headers.ContentDisposition.Name.Trim('\"'),
+        //                        multipartContent.ReadAsByteArrayAsync().Result,
+        //                        multipartContent.Headers.ContentDisposition.FileName.Trim('\"'),
+        //                        multipartContent.Headers.ContentType.MediaType);
+        //                    var result = await _restClient.ExecuteTaskAsync<ResultViewModel>(restRequest);
+        //                    if (!result.IsSuccessful || result.Data.Validate == 0)
+        //                        return result.Data;
+        //                }
+        //                catch (Exception exception)
+        //                {
+        //                    Logger.Log(exception, logType: LogType.Debug);
+        //                }
+        //            }
+        //        }
+        //        catch (Exception exception)
+        //        {
+        //            Logger.Log(exception, logType: LogType.Debug);
+        //            throw;
+        //        }
+
+        //        return new ResultViewModel { Validate = 1, Code = 200, Id = deviceId, Message = "Files uploaded and upgrading firmware started." };
+        //    });
+        //}
+
+
+    }
+}
