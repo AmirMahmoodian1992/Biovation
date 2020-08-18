2706e9131981557d4a3e661f7b04899aa7571dfb
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-08-18 19:17:28 +0430

    [Domain]: Add Model

---
+++ Biovation.Domain/ResultViewModelForSelectByPaging.cs
@@ -0,0 +1,17 @@
+﻿using Biovation.Domain.DataMappers;
+using DataAccessLayerCore.Attributes;
+
+namespace Biovation.Domain
+{
+  
+    public class ResultViewModelForSelectByPaging<T>
+    {
+        [DataMapper(Mapper = typeof(IntToLongMapper))]
+        public T Data { get; set; }
+        public int From { get; set; }
+        public int PageNumber { get; set; }
+        public int Size { get; set; }
+        public int Count { get; set; }
+
+    }
+}
