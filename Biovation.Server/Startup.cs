d6a0c29a1289c29015c93301292c82aea6de14d5
arash <sarhadiarash@yahoo.com>
2020-08-22 14:19:06 +0430

    [Server] : Add name for API

--- Biovation.Server/Startup.cs
+++ Biovation.Server/Startup.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Biovation.CommonClasses.Manager;
 using Biovation.Constants;
 using Biovation.Repository;
@@ -14,14 +9,11 @@ using DataAccessLayerCore.Domain;
 using DataAccessLayerCore.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using MoreLinq.Extensions;
 
 namespace Biovation.Server
 {
@@ -163,7 +155,7 @@ namespace Biovation.Server
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Biovation API");
             });
 
 
