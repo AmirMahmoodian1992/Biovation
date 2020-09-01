270e87cf07c46c6bb8b33e3d6e02c2b38429e867
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-09-01 18:48:45 +0430

    [Queries]: Change lookups For V2

--- Biovation.Repository/SQL/v2/LookupRepository.cs
+++ Biovation.Repository/SQL/v2/LookupRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using Biovation.Domain;
 using DataAccessLayerCore.Extentions;
@@ -15,14 +16,18 @@ namespace Biovation.Repository.SQL.v2
             _repository = repository;
         }
 
-        public ResultViewModel<PagingResult<Lookup>> GetLookups(string code = default, string name = default, int lookupCategoryId = default, string codePrefix = default)
+        public ResultViewModel<PagingResult<Lookup>> GetLookups(string code = default, string name = default, int lookupCategoryId = default, string codePrefix = default, int pageNumber = default,
+            int pageSize = default)
         {
             var parameters = new List<SqlParameter>
             {
                 new SqlParameter("@code", code),
                 new SqlParameter("@name", name),
                 new SqlParameter("@lookupCategoryId", lookupCategoryId),
-                new SqlParameter("@codePrefix", codePrefix)
+                new SqlParameter("@codePrefix", codePrefix),
+                new SqlParameter("@PageNumber", SqlDbType.Int) {Value = pageNumber},
+                new SqlParameter("@PageSize", SqlDbType.Int) {Value = pageSize},
+
             };
 
             return _repository.ToResultList<PagingResult<Lookup>>("SelectLookups", parameters).FetchFromResultList();
