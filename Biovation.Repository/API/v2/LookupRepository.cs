469d50094b703ceafcc4d32d1c5a361627f5b583
arash <sarhadiarash@yahoo.com>
2020-09-02 00:56:33 +0430

    [Repository]: Keep going....

--- Biovation.Repository/API/v2/LookupRepository.cs
+++ Biovation.Repository/API/v2/LookupRepository.cs
@@ -18,7 +18,20 @@ namespace Biovation.Repository.API.v2
             _restClient = restClient;
         }
 
-        
+        public ResultViewModel<PagingResult<Lookup>> GetLookups(string code = default, string name = default,
+            int lookupCategoryId = default, string codePrefix = default, int pageNumber = default,
+            int pageSize = default)
+        {
+            var restRequest = new RestRequest($"Queries/v2/Lookup", Method.GET);
+            var requestResult = _restClient.ExecuteAsync<ResultViewModel<PagingResult<Lookup>>>(restRequest);
+            restRequest.AddQueryParameter("code", code.ToString());
+            restRequest.AddQueryParameter("name", name.ToString());
+            restRequest.AddQueryParameter("lookupCategoryId", lookupCategoryId.ToString());
+            restRequest.AddQueryParameter("codePrefix", codePrefix.ToString());
+            restRequest.AddQueryParameter("pageNumber", pageNumber.ToString());
+            restRequest.AddQueryParameter("pageSize", pageSize.ToString());
+            return requestResult.Result.Data;
+        }
 
 
     }
---
+++ Biovation.Service/API/v2/LookupService.cs
@@ -0,0 +1,24 @@
+﻿using Biovation.Domain;
+using Biovation.Repository.API.v2;
+
+namespace Biovation.Service.API.v2
+{
+    public class LookupService
+    {
+        private readonly LookupRepository _lookupRepository;
+
+        public LookupService(LookupRepository lookupRepository)
+        {
+            _lookupRepository = lookupRepository;
+        }
+
+        public ResultViewModel<PagingResult<Lookup>> GetLookups(string code = default, string name = default,
+            int lookupCategoryId = default, string codePrefix = default, int pageNumber = default,
+            int pageSize = default)
+        {
+            return _lookupRepository.GetLookups(code, name, lookupCategoryId, codePrefix, pageNumber, pageSize);
+        }
+
+
+    }
+}
