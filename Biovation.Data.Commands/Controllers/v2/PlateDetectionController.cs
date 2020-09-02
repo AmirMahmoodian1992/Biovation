2c3eec8a0b01e947e900e5d360c79a6f0e8e7f92
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-09-02 15:46:14 +0430

    [Commands]:Plate Detection v2

---
+++ Biovation.Data.Commands/Controllers/v2/PlateDetectionController.cs
@@ -0,0 +1,39 @@
+﻿using Biovation.Domain;
+using Biovation.Repository.SQL.v2;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Biovation.Data.Queries.Controllers.v2
+{
+
+    [Route("biovation/api/queries/v2/[controller]")]
+    //[ApiVersion("2.0")]
+    public class PlateDetectionController : Controller
+    {
+
+        private readonly PlateDetectionRepository _plateDetectionRepository;
+
+        public PlateDetectionController(PlateDetectionRepository PlateDetectionRepository)
+        {
+            _plateDetectionRepository = PlateDetectionRepository;
+        }
+
+
+        [HttpPost]
+        [Route("AddLicensePlate")]
+        public Task<ResultViewModel> AddLicensePlate(LicensePlate licensePlate = default)
+        {
+            return Task.Run(() => _plateDetectionRepository.AddLicensePlate(licensePlate));
+        }
+
+        [HttpPost]
+        [Route("AddPlateDetectionLog")]
+        public Task<ResultViewModel> AddPlateDetectionLog(PlateDetectionLog log)
+        {
+            return Task.Run(() => _plateDetectionRepository.AddPlateDetectionLog(log));
+        }
+
+
+    }
+}
