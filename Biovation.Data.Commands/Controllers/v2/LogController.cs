59cf46cdaeebdbe56a874f7df7c8ce50c9fbc35a
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-09-02 15:38:24 +0430

    [Commands]:Logs

---
+++ Biovation.Data.Commands/Controllers/v2/LogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Biovation.Domain;
+using Biovation.Repository.SQL.v2;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Biovation.Data.Commands.Controllers.v2
+{
+    [Route("biovation/api/commands/v2/[controller]")]
+    public class LogController : Controller
+    {
+
+        private readonly LogRepository _logRepository;
+
+
+        public LogController(LogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+
+    }
+}
