b88e2762cc6ecfd9f10638a7ad2bfc7ac4c4697b
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-09-02 14:11:20 +0430

    [Queries]: FaceTemplate

--- Biovation.Data.Commands/Controllers/v2/FaceTemplateController.cs
+++ Biovation.Data.Commands/Controllers/v2/FaceTemplateController.cs
@@ -1,15 +1,12 @@
 ﻿using Biovation.Domain;
 using Biovation.Repository.SQL.v2;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 
 namespace Biovation.Data.Commands.Controllers.v2
 {
-    //[Route("Biovation/Api/{controller}/{action}", Name = "Device")]
-    //[Route("biovation/api/v{version:apiVersion}/[controller]")]
+
     [Route("biovation/api/commands/v2/[controller]")]
     //[ApiVersion("1.0")]
     public class FaceTemplateController : Controller
@@ -22,9 +19,6 @@ namespace Biovation.Data.Commands.Controllers.v2
             _faceTemplateRepository = faceTemplateRepository;
         }
 
-
-       
-       
         [HttpPut]
         [Route("ModifyFaceTemplate")]
         public Task<ResultViewModel> ModifyFaceTemplate(FaceTemplate faceTemplate)
@@ -34,11 +28,11 @@ namespace Biovation.Data.Commands.Controllers.v2
 
         [HttpDelete]
         [Route("DeleteFaceTemplate")]
-        public Task<ResultViewModel> DeleteFaceTemplate(long userId = 0 , int index = 0)
+        public Task<ResultViewModel> DeleteFaceTemplate(long userId = 0, int index = 0)
         {
-            return Task.Run(() => _faceTemplateRepository.DeleteFaceTemplate(userId ,index));
+            return Task.Run(() => _faceTemplateRepository.DeleteFaceTemplate(userId, index));
         }
 
- 
+
     }
 }
