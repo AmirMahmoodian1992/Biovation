fd24c7dc5a83a2ff3f91f67702e4922ff540dfd3
Mahdieh Heidari Dahoii <mahdiehheidari93@gmail.com>
2020-09-02 16:08:48 +0430

    [Commands]: TimeZOne

---
+++ Biovation.Data.Commands/Controllers/v2/TimeZoneController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Biovation.Repository.SQL.v2;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Biovation.Domain;
+using TimeZone = Biovation.Domain.TimeZone;
+
+namespace Biovation.Data.Queries.Controllers.v2
+{
+    [Route("biovation/api/v{version:apiVersion}/[controller]")]
+    
+    public class TimeZoneController : Controller
+    {
+
+        private readonly TimeZoneRepository _timeZoneRepository;
+
+        public TimeZoneController(TimeZoneRepository timeZoneRepository)
+        {
+            _timeZoneRepository = timeZoneRepository;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task<ResultViewModel<TimeZone>> TimeZones(int id = default)
+        {
+            return Task.Run(() => _timeZoneRepository.GetTimeZone(id));
+        }
+
+        [HttpPut]
+        [Route("ModifyTimeZone")]
+        public Task<ResultViewModel> ModifyTimeZone(TimeZone timeZone)
+        {
+            return Task.Run(() => _timeZoneRepository.ModifyTimeZone(timeZone));
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Task<ResultViewModel> DeleteTimeZone(int id)
+        {
+            return Task.Run(() => _timeZoneRepository.DeleteTimeZone(id));
+        }
+
+
+    }
+}
