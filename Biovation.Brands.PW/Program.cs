9cbf04cd0b6472a5301e568d7914bcd391922166
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2020-10-05 14:46:57 +0330

    [PW]: Adding services and configuring the program host

--- Biovation.Brands.PW/Program.cs
+++ Biovation.Brands.PW/Program.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Biovation.Brands.PW
 {
@@ -18,6 +22,19 @@ namespace Biovation.Brands.PW
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureMetricsWithDefaults(
+                    builder =>
+                    {
+                        builder.Report.OverHttp(options => {
+                            options.HttpSettings.RequestUri = new Uri("http://localhost:9038/biovation/api/dashboard/metrics");
+                            options.HttpPolicy.BackoffPeriod = TimeSpan.FromSeconds(30);
+                            options.HttpPolicy.FailuresBeforeBackoff = 2;
+                            options.HttpPolicy.Timeout = TimeSpan.FromSeconds(10);
+                            options.MetricsOutputFormatter = new MetricsJsonOutputFormatter();
+                            options.FlushInterval = TimeSpan.FromSeconds(20);
+                        });
+                    })
+                .UseSerilog().UseMetrics()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Biovation.Brands.PW/Startup.cs
+++ Biovation.Brands.PW/Startup.cs
@@ -1,7 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Extensions.Configuration;
+using Biovation.Brands.PW.Service;
+using Biovation.CommonClasses;
+using Biovation.CommonClasses.Manager;
+using Biovation.Constants;
+using Biovation.Repository.Api.v2;
+using Biovation.Service.Api.v1;
+using DataAccessLayerCore;
+using DataAccessLayerCore.Domain;
+using DataAccessLayerCore.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,23 +22,117 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestSharp;
+using Serilog;
 
 namespace Biovation.Brands.PW
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public BiovationConfigurationManager BiovationConfiguration { get; set; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
         {
             Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration)
+                .Enrich.With(new ThreadIdEnricher())
+                .Enrich.WithProperty("Version", Assembly.GetExecutingAssembly().GetName().Version)
+                .CreateLogger();
+
+            BiovationConfiguration = new BiovationConfigurationManager(configuration);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            var metrics = new MetricsBuilder()
+                .Configuration.ReadFrom(configuration);
+
+            Configuration = builder.Build();
+            metrics.Build();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new TimeSpanToStringConverter());
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                }).AddMetrics();
+
+            services.AddHealthChecks();
+
+            services.AddSingleton(BiovationConfiguration);
+            services.AddSingleton(BiovationConfiguration.Configuration);
+
+            ConfigureRepositoriesServices(services);
         }
+
+        private void ConfigureRepositoriesServices(IServiceCollection services)
+        {
+            var connectionInfo = new DatabaseConnectionInfo
+            {
+                ProviderName = BiovationConfiguration.ConnectionStringProviderName(),
+                WorkstationId = BiovationConfiguration.ConnectionStringWorkstationId(),
+                InitialCatalog = BiovationConfiguration.ConnectionStringInitialCatalog(),
+                DataSource = BiovationConfiguration.ConnectionStringDataSource(),
+                Parameters = BiovationConfiguration.ConnectionStringParameters(),
+                UserId = BiovationConfiguration.ConnectionStringUsername(),
+                Password = BiovationConfiguration.ConnectionStringPassword()
+            };
+
+            services.AddSingleton(connectionInfo);
+            services.AddSingleton<IConnectionFactory, DbConnectionFactory>();
+
+            var restClient = (RestClient)new RestClient($"http://localhost:{BiovationConfigurationManager.BiovationWebServerPort}/biovation/api").UseSerializer(() => new RestRequestJsonSerializer());
+            services.AddSingleton(restClient);
+
+            services.AddSingleton<GenericRepository, GenericRepository>();
+            services.AddSingleton<AccessGroupService, AccessGroupService>();
+            services.AddSingleton<AdminDeviceService, AdminDeviceService>();
+            services.AddSingleton<BlackListService, BlackListService>();
+            services.AddSingleton<DeviceGroupService, DeviceGroupService>();
+            services.AddSingleton<DeviceService, DeviceService>();
+            services.AddSingleton<FaceTemplateService, FaceTemplateService>();
+            services.AddSingleton<FingerTemplateService, FingerTemplateService>();
+            services.AddSingleton<GenericCodeMappingService, GenericCodeMappingService>();
+            services.AddSingleton<LogService, LogService>();
+            services.AddSingleton<LookupService, LookupService>();
+            services.AddSingleton<SettingService, SettingService>();
+            services.AddSingleton<TaskService, TaskService>();
+            services.AddSingleton<TimeZoneService, TimeZoneService>();
+            services.AddSingleton<UserCardService, UserCardService>();
+            services.AddSingleton<UserGroupService, UserGroupService>();
+            services.AddSingleton<UserService, UserService>();
+            services.AddSingleton<PwLogService, PwLogService>();
+
+            services.AddSingleton<AccessGroupRepository, AccessGroupRepository>();
+            services.AddSingleton<AdminDeviceRepository, AdminDeviceRepository>();
+            services.AddSingleton<BlackListRepository, BlackListRepository>();
+            services.AddSingleton<DeviceGroupRepository, DeviceGroupRepository>();
+            services.AddSingleton<DeviceRepository, DeviceRepository>();
+            services.AddSingleton<FaceTemplateRepository, FaceTemplateRepository>();
+            services.AddSingleton<FingerTemplateRepository, FingerTemplateRepository>();
+            services.AddSingleton<GenericCodeMappingRepository, GenericCodeMappingRepository>();
+            services.AddSingleton<LogRepository, LogRepository>();
+            services.AddSingleton<LookupRepository, LookupRepository>();
+            services.AddSingleton<SettingRepository, SettingRepository>();
+            services.AddSingleton<TaskRepository, TaskRepository>();
+            services.AddSingleton<TimeZoneRepository, TimeZoneRepository>();
+            services.AddSingleton<UserCardRepository, UserCardRepository>();
+            services.AddSingleton<UserGroupRepository, UserGroupRepository>();
+            services.AddSingleton<UserRepository, UserRepository>();
+
+            services.AddSingleton<Lookups, Lookups>();
+            services.AddSingleton<GenericCodeMappings, GenericCodeMappings>();
+        }   
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
