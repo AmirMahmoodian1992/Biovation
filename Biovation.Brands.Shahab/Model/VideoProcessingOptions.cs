c11e72a24e0134e7260c74b0727afc86e488f2b6
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2020-10-05 21:06:28 +0330

    [Shahab]: Adding services and configuring the program host

---
+++ Biovation.Brands.Shahab/Model/VideoProcessingOptions.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Biovation.Brands.Shahab.Model
+{
+    public struct VideoProcessingOptions
+    {
+        public short ResizeThresh;     // if width of input image is larger than this, it will be resized
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public byte[] NumValidChars;   // Number of valid characters usuallay {8, 0}. if e.g. 5 character plates are also available, use {8, 5}
+        //public byte num_valid_chars1;   //if Two types of plates are important
+        //public byte num_valid_chars2;   //if Two types of plates are important
+        public byte medianKernel;       // (0: no kernel) (3, 5, 7 ... median kernel of this size)
+        public byte SavePlateOption;  //save_plate_option: 0 don't save anything, 1: save plate only, 2: save whole car image and plate							
+                                      //اگر عدد صفر انتخاب شود، فقط رشته پلاک و مستطیل آن گزارش شده و تصویر بریده شده پلاک ارسال نمی شود
+                                      //عدد 2 سبب استفاده بیشتر از حافظه و کاهش حدود 5درصدی سرعت پلاک خوانی می شود
+        public short VlcNetCacheTime;
+        //Limits of character dimensions
+        public byte MinCharW;  //minimum with of characters
+        public byte MinCharH;  //minimum height of characters
+        public byte MaxCharW;  //maximum with of characters
+        public byte MaxCharH;  //maximum height of characters
+
+        public float SkewCoefficient;         //more value means more skew: successive characters are not in the same Y position
+
+        public byte IgnoreInvertedPlates;//may not be used
+        public byte DetectMotor; //if 1 motor detection is enabled, if 0 No.
+
+        //ب) پارامترهای ویدیو
+        public byte NFrmSkipOnSuccess;  //Number of frames to be skipped after successful plate detection
+        public byte DiffThresh;            //Difference threshold between current frame and background to suppose entrance of new car 
+        public byte PlateBufSize;         // Buffer length of recent successive plates (max = 50). 
+        public byte DetectMultiPlate;
+        public byte SkipSamePlateTime;// don't report same plate until "some time" elpased
+
+        public byte PlayAudioFromCamera; //in vlc mode we can play audio (from version 7.45)
+
+        public byte PlateType; //0: Only Iran standard, 1: + Arvand, 2: + Arg 
+        public byte Reserved2; //
+
+        //internal byte horizontal_thresh;
+        // از این دو پارامتر برای تعیین ناحیه پردازش استفاده میشود
+        public byte MinThreshHist;
+        public byte MaxThreshHist;
+
+        //تنظیمات پیشرفته که به ندرت نیاز است تغییر داده شود
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public byte[] blur_kernel;//{ 13, 13 }; //Size of blur kernel used for binarization. Default is 13x13. To handle shadow, try 13x1 or 13x3	
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public float[] ImgBinTh;//[2] = { 0.9f, 0.95f }; //Adaptive Binarization Threshold (between 0.5 and 1) default is [0.95, 0.9]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public float[] PltBinTh;//[4] = { 0.8f, 0.85f, 0.92f, 1.0f }; //Adaptive Binarization Threshold (between 0.5 and 1) default is [0.95, 0.9]
+
+        public static void SetDefaultOptions(byte instance, VideoProcessingOptions options)
+        {
+            options.MinCharW = 5; //minimum with of characters
+            options.MinCharH = 7; //minimum height of characters
+            options.MaxCharW = 100; //maximum with of characters
+            options.MaxCharH = 100; //maximum height of characters
+            options.SkewCoefficient = 1.0f; //more value means more skew: successive characters are not in the same Y position
+            options.ResizeThresh = 1100;//if width of input image is larger than this, it will be resized
+            options.medianKernel = 0;//Kernel size: 0, 3, 5, 7, etc...
+            options.IgnoreInvertedPlates = 0;
+            options.DetectMotor = 0; //if 1 motor detection is enabled, if 0 No.
+            options.DetectMultiPlate = 1;
+            options.NumValidChars = new byte[] { 8, 0 };
+            //options.num_valid_chars1 = 8; //5 for free 
+            //options.num_valid_chars2 = 0; //5 for free //mhh - changed 5 to 0
+
+            options.SavePlateOption = (byte)ESAVE_PLATE_OPTION.SAVE_PLATE_AND_CAR;
+            options.NFrmSkipOnSuccess = 10;
+            options.PlateBufSize = 12;
+            options.Reserved2 = 0; // car recognition is disabled
+            options.VlcNetCacheTime = 1000;
+            options.PlateType = 0;
+            options.DiffThresh = 10; //difference threshold between current frame and background to suppose entrance of new car 
+            options.PlayAudioFromCamera = 0;
+            options.MinThreshHist = 60;
+            options.MaxThreshHist = 170;
+
+            options.blur_kernel = new byte[] { 13, 13 };
+            options.ImgBinTh = new[] { 0.9f, 0.95f };
+            options.PltBinTh = new[] { 0.8f, 0.85f, 0.92f, 1.0f };
+            ShahabApi.anpr_set_params(instance, ref options);
+        }
+    }
+}
---
+++ Biovation.Brands.Shahab/SahabApi.cs
@@ -0,0 +1,150 @@
+﻿using Biovation.Brands.Shahab.Model;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Biovation.Brands.Shahab
+{
+    enum ESAVE_PLATE_OPTION { SAVE_NOTHING, SAVE_PLATE_ONLY, SAVE_PLATE_AND_CAR };
+
+
+    public class ShahabApi
+    {
+        public delegate void ANPR_EVENT_CALLBACK(int event_type, byte stream, byte plt_idx);
+
+        public const string DLL_NAME = "ANPR.dll";
+        public const int WM_USER = 0x0400;
+        public const int WM_NEW_FRAME = WM_USER + 100;
+        public const int WM_SCENE_CHANGED = WM_USER + 101;
+        public const int WM_PLATE_DETECTED = WM_USER + 102;
+        public const int WM_PLATE_NOT_DETECTED = WM_USER + 103; //when a car is in the field of camera but its plate is not recognized
+        public const int WM_END_OF_VIDEO = WM_USER + 104; //when video file finished or camera closed
+        public const int WM_CONNECTED = WM_USER + 105; //Connected to camera (or video file) from Ver 8.43
+
+        //هنگامی که اولین پلاک در صحنه دیده می شود، برای ترسیم مستطیل اطراف آن
+        //رویداد تشخیص قطعی پلاک شماره 102 است
+        public const int WM_INITIAL_PLATE = WM_USER + 108;
+        public const int WM_CAM_NOT_FOUND = WM_USER + 109;
+
+        //1
+        //تابع زیر به ازای هر نسخه کتابخانه (مثلا به ازای هر دوربین) حتما باید یکبار فراخوانی شود. 
+        //این تابع شبکه های عصبی مورد استفاده را بارگذاری می کند
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_create(byte instance, [MarshalAs(UnmanagedType.LPWStr)] string security_code, byte log_level = 1, [MarshalAs(UnmanagedType.LPWStr)] string cfg_file = null);
+
+        //1-1
+        //این تابع مدیریت تمام رویدادهای کتابخانه را بر عهده دارد
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_set_event_callback(ANPR_EVENT_CALLBACK callback_fcn);
+
+        //2
+        //این تابع مسیر فایل تصویری را دریافت کرده و نتیجه را بر می گرداند: 
+        //رشته، میزان اطمینان به رشته حاصله و مستطیل پلاک
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_recognize(byte instance, [MarshalAs(UnmanagedType.LPWStr)] string fn,
+            [MarshalAs(UnmanagedType.LPWStr)] string result, ref float cnf, ref RectangleCoordinates prc);
+
+        //3
+        //این تابع مانند تابع بالایی است با این تفاوت که اندیس مستطیل مورد علاقه را هم می گیرد.
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_recognizeROI(byte instance, byte roi_idx, [MarshalAs(UnmanagedType.LPWStr)] string fn,
+            [MarshalAs(UnmanagedType.LPWStr)] string result, ref float cnf, ref RectangleCoordinates prc);
+
+
+        //4
+        //تابع زیر برای بافری است که از دوربین یا فایل گرفته اید و نوعا یک جریان فشرده مثل جی پگ است.
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_recognize_stream(byte instance, IntPtr compressed_stream, int size, [MarshalAs(UnmanagedType.LPWStr)] string result, ref float cnf, ref RectangleCoordinates prc);
+
+        //5
+        //تابع زیر برای زمانی است که بایتهای تصویر به صورت فشرده نشده در آرایه ای قرار دارند
+        //مثلا اشاره گر ابتدای یک بیت مپ
+        //مثال آن در همین برنامه دیده می شود
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_recognize_buffer(byte instance, IntPtr bytes, int W, int H, int step, [MarshalAs(UnmanagedType.LPWStr)] string result, ref float cnf, ref RectangleCoordinates prc);
+
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_recognize_bufferROI(byte instance, byte roi_idx, IntPtr bytes, int W, int H, int step, [MarshalAs(UnmanagedType.LPWStr)] string result, ref float cnf, ref RectangleCoordinates prc);
+
+        //6
+        //خروجی تابع 2 یک رشته فارسی یونیکد است، اگر خروجی انگلیسی «اسکی» را لازم دارید از این تابع استفاده کنید 
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_get_ascii_result([MarshalAs(UnmanagedType.LPWStr)] string result_fa, [MarshalAs(UnmanagedType.LPStr)] string result_en);//Get ascii results in English
+
+        //7
+        //خروجی تابع 2 یک رشته فارسی یونیکد است، اگر خروجی انگلیسی «یونیکد» را لازم دارید از این تابع استفاده کنید 
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_get_en_result([MarshalAs(UnmanagedType.LPWStr)] string result_fa, [MarshalAs(UnmanagedType.LPWStr)] string result_en);//Get unicode results in English
+
+        //8
+        //یافتن نویسه ها از بافر حافظه ای که تنها شامل تصویر پلاک است
+        //به عبارتی محل پلاک باید قبلا یافت شده باشد
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_find_chars(byte instance, IntPtr bytes, int W, int H, int step, RectangleCoordinates roi, [MarshalAs(UnmanagedType.LPWStr)] string result, ref float pcnf);
+
+        //9
+        //این تابع برای تنظیم پارامترهای کتابخانه است.
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_set_params(byte instance, ref VideoProcessingOptions slpr_params);
+
+        //این تابع برای تست برخی پارامترهای کتابخانه است و حتی الامکان نباید استفاده شود.
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_set_debug_mode(byte instance, byte debug_level);
+
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_add_ROI(byte instance, RectangleCoordinates roi);
+
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_clear_ROIs(byte instance);
+
+        [DllImport(DLL_NAME)]
+        public static extern short anpr_get_plate(byte instance, byte plate_idx, ref PlateResult result);
+
+        [DllImport(DLL_NAME)]
+        public static extern void anpr_about();
+
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_start_grabbing(byte instance, [MarshalAs(UnmanagedType.LPStr)] string URL, byte interval_ms, IntPtr hwndDraw, byte take_shots, byte draw_method);
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_stop_grabbing(byte instance);
+
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_start_grabbingVLC(byte instance, [MarshalAs(UnmanagedType.LPStr)] string URL, byte interval_ms, IntPtr hwndDraw, byte take_shots, byte draw_method);
+
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_stop_grabbingVLC(byte instance);
+
+
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_pause_or_resume(byte instance, byte pause);
+
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_get_frame_info(byte instance, ref int W, ref int H, ref int channels, ref int step);
+
+        [DllImport(DLL_NAME)]
+        public static extern IntPtr vlpr_get_frame(byte instance);
+
+        //Start Processing of Camera Frames
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_start_process(byte instance);
+
+        //Stop Processing of Camera Frames
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_stop_process(byte instance);
+
+        //str must be allocated before
+        //output is buffer of plate image
+        //پلاک پس از عبور خودرو گزارش می شود. لذا به منظور ثبت تصویر پلاک
+        //بافر آن نگهداری می شود
+        [Obsolete("vlpr_get_last_resultsW is deprecated, please use anpr_get_plate instead.")]
+        [DllImport(DLL_NAME)]
+        public static extern IntPtr vlpr_get_last_resultsW(byte stream, [MarshalAs(UnmanagedType.LPWStr)] string str, ref RectangleCoordinates pr, ref float cnf, ref IntPtr img_car_buffer, ref byte direction);
+
+        [Obsolete("vlpr_get_last_results is deprecated, please use anpr_get_plate instead.")]
+        [DllImport(DLL_NAME)]
+        public static extern IntPtr vlpr_get_last_results(byte stream, byte[] str, ref RectangleCoordinates pr, ref float cnf, ref IntPtr img_car_buffer, ref byte direction);
+
+        //Recognize Last Frame Grabbed from camera or video file
+        [DllImport(DLL_NAME)]
+        public static extern short vlpr_recognize_cur_frame(byte instance, [MarshalAs(UnmanagedType.LPWStr)] string str, ref RectangleCoordinates pr, ref float cnf);
+    }
+}
