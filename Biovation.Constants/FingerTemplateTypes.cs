304c7441a4cedfd2cea75e29a59b88016a778164
mahdieh heidari <mahdiehheidari93@gmail.com>
2020-11-08 00:07:36 +0330

    [Queries]: Add Finger Template Type Constant Value

--- Biovation.Constants/FingerTemplateTypes.cs
+++ Biovation.Constants/FingerTemplateTypes.cs
@@ -8,17 +8,20 @@ namespace Biovation.Constants
         public const string V400Code = "18001";
         public const string SU384Code = "18003";
         public const string VX10Code = "18002";
+        public const string EOSSupremaCode = "18004";
 
         public FingerTemplateTypes(Lookups lookups)
         {
             V400 = lookups.FingerTemplateType.FirstOrDefault(lookup => string.Equals(lookup.Code, V400Code));
             SU384 = lookups.FingerTemplateType.FirstOrDefault(lookup => string.Equals(lookup.Code, SU384Code));
             VX10 = lookups.FingerTemplateType.FirstOrDefault(lookup => string.Equals(lookup.Code, VX10Code));
+            EOSSuprema = lookups.FingerTemplateType.FirstOrDefault(lookup => string.Equals(lookup.Code, EOSSupremaCode));
         }
 
         public Lookup V400;
         public Lookup SU384;
         public Lookup VX10;
+        public Lookup EOSSuprema;
 
     }
 }
