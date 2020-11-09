4fc38d595ce1ab704603c33b830c3497cfd93d9a
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2020-11-10 02:22:56 +0330

    [Tools]: Fix program startup

--- Biovation.Tools.UserAdapter/Program.cs
+++ Biovation.Tools.UserAdapter/Program.cs
@@ -17,7 +17,7 @@ namespace Biovation.Tools.UserAdapter
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new UserAdaptationForm());
         }
     }
 }
