d0869084ffaa7987e5e740140a5837da43a99c12
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2020-12-28 18:06:23 +0330

    [Common]: Fix signature

--- Biovation.Service.Api/v2/RelayController/EntranceService.cs
+++ Biovation.Service.Api/v2/RelayController/EntranceService.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Biovation.Domain;
+﻿using Biovation.Domain;
 using Biovation.Domain.RelayControllerModels;
 using Biovation.Repository.Sql.v2.RelayController;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Biovation.Service.Api.v2.RelayController
 {
@@ -20,11 +20,10 @@ namespace Biovation.Service.Api.v2.RelayController
             return Task.Run(() => _entranceRepository.CreateEntrance(entrance));
         }
 
-        public Task<ResultViewModel<PagingResult<Entrance>>> GetEntrances(List<DeviceBasicInfo> devices, List<Scheduling> schedulings, int id = 0,
-            string name = null, string description = null, int pageNumber = 0,
-            int pageSize = 0, int nestingDepthLevel = 4)
+        public Task<ResultViewModel<PagingResult<Entrance>>> GetEntrances(int id = 0, string name = null, string description = null, int pageNumber = 0,
+            int pageSize = 0, int nestingDepthLevel = 4, List<DeviceBasicInfo> devices = null, List<Scheduling> schedulings = null)
         {
-            return Task.Run(() => _entranceRepository.GetEntrances(devices, schedulings, id, name,description, pageNumber, pageSize, nestingDepthLevel));
+            return Task.Run(() => _entranceRepository.GetEntrances(devices, schedulings, id, name, description, pageNumber, pageSize, nestingDepthLevel));
         }
 
         public Task<ResultViewModel> UpdateEntrance(Entrance entrance)
