63206567e83c2daadc264eda71720ce9527dd1da
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-02-07 19:07:48 +0330

    [Virdi]: Execute offline tasks based on connected device Id

---
+++ Biovation.Brands.Virdi/Controllers/VirdiTaskController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Biovation.Brands.Virdi.Manager;
+using Biovation.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biovation.Brands.Virdi.Controllers
+{
+    [ApiController]
+    [Route("Biovation/Api/[Controller]")]
+    public class VirdiTaskController : ControllerBase
+    {
+        private readonly TaskManager _taskManager;
+
+        public VirdiTaskController(TaskManager taskManager)
+        {
+            _taskManager = taskManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("[Action]")]
+        public async Task<ResultViewModel> RunProcessQueue(int deviceId = default)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    _taskManager.ProcessQueue(deviceId);
+                    return new ResultViewModel { Success = true };
+                }
+                catch (Exception exception)
+                {
+                    return new ResultViewModel { Success = false, Message = exception.ToString() };
+                }
+            });
+        }
+    }
+}
