c026de2ad67c591698648fc7a70280eb8371e0ea
Kourosh Sajjadi <kooroshsajadi@gmail.com>
2021-04-21 11:21:52 +0430

    [Paliz]: Added 'DeviceMultipleEventArgs' in the 'Model/Logs' route

---
+++ Biovation.Brands.Paliz/Model/logs/DeviceLogEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using PalizTiara.Api.Models;
+
+namespace Biovation.Brands.Paliz.Model.logs
+{
+    public class DeviceLogEventArgs
+    {
+        public DeviceLogEventArgs()
+        {
+
+        }
+        public DeviceLogEventArgs(DeviceLogModel deviceLogModel, bool result)
+        {
+
+        }
+
+        public bool Result { get; set; }
+        public DeviceLogModel DeviceLogModel { get; set; }
+    }
+}
