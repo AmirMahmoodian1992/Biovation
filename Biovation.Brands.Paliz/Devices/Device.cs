438b9f9c4349f89bc23449a06c0795f186f3c53c
Kourosh Sajjadi <kooroshsajadi@gmail.com>
2021-04-21 10:41:34 +0430

    [Paliz]: Initialized 'Device' and 'DeviceFactory' models.

---
+++ Biovation.Brands.Paliz/Devices/Device.cs
@@ -0,0 +1,14 @@
+﻿using Biovation.Brands.Paliz.Model;
+using Biovation.CommonClasses;
+using Biovation.CommonClasses.Interface;
+using Biovation.Domain;
+using Biovation.Service.Api.v1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+
+namespace Biovation.Brands.Paliz.Devices
+{
+    
+}
