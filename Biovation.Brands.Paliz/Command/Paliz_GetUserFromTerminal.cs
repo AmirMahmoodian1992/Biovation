abe26b514de47e5f8e15eaf382b39a8323c3bdb9
Kourosh Sajjadi <kooroshsajadi@gmail.com>
2021-04-26 11:52:08 +0430

    [Paliz]: Added a new class for getting a specified user command

---
+++ Biovation.Brands.Paliz/Command/Paliz_GetUserFromTerminal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biovation.Brands.Paliz.Command
+{
+    public class PalizGetUserFromTerminal
+    {
+    }
+}
