b0d775284c91e63b15f36f0f6ce0c8ebe3d4d376
Kourosh Sajjadi <kooroshsajadi@gmail.com>
2021-04-28 13:39:44 +0430

    [Paliz]: Added device name nad device info cod mappings class

---
+++ Biovation.Brands.Paliz/Manager/PalizDeviceMappings.cs
@@ -0,0 +1,23 @@
+﻿using Biovation.Domain;
+using System.Collections.Generic;
+
+namespace Biovation.Brands.Paliz.Manager
+{
+    public class PalizDeviceMappings
+    {
+        private readonly Dictionary<string, DeviceBasicInfo> _deviceMappings;
+
+        public DeviceBasicInfo GetDeviceBasicInfo(string deviceName)
+        {
+            return _deviceMappings.ContainsKey(deviceName) ? _deviceMappings[deviceName] : null;
+        }
+
+        public void InsertDeviceMapping(string deviceName, DeviceBasicInfo device)
+        {
+            if (!_deviceMappings.ContainsKey(deviceName))
+            {
+                _deviceMappings.Add(deviceName, device);
+            }
+        }
+    }
+}
