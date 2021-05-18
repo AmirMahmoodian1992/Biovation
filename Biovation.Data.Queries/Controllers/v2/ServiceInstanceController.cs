7e48eea6879b92b1bbcb151010d6fb86a87e4fe0
ASarhadi <ASarhadi@PT-ASARHADI>
2021-05-18 13:23:00 +0430

    [Queries]: fix bug

--- Biovation.Data.Queries/Controllers/v2/ServiceInstanceController.cs
+++ Biovation.Data.Queries/Controllers/v2/ServiceInstanceController.cs
@@ -3,7 +3,7 @@ using Biovation.Domain;
 using Biovation.Repository.Sql.v2;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Biovation.Data.Commands.Controllers.v2
+namespace Biovation.Data.Queries.Controllers.v2
 {
     [ApiController]
     [Route("biovation/api/v2/[controller]")]
