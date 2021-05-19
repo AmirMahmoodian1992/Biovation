6fab1133a71aa180a81f3c89df52fca1e8eb86b9
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-05-19 08:42:51 +0430

    [Brands]: Fix excluded tasks statuses

--- Biovation.Brands.PW/Manager/TaskManager.cs
+++ Biovation.Brands.PW/Manager/TaskManager.cs
@@ -104,7 +104,11 @@ namespace Biovation.Brands.PW.Manager
         public async Task ProcessQueue(int deviceId = default, CancellationToken cancellationToken = default)
         {
             var allTasks = await _taskService.GetTasks(brandCode: DeviceBrands.ProcessingWorldCode, deviceId: deviceId,
-                excludedTaskStatusCodes: new List<string> { TaskStatuses.DoneCode, TaskStatuses.FailedCode });
+                excludedTaskStatusCodes: new List<string>
+                {
+                    TaskStatuses.DoneCode, TaskStatuses.FailedCode, TaskStatuses.RecurringCode,
+                    TaskStatuses.ScheduledCode, TaskStatuses.InProgressCode
+                });
 
             lock (_tasks)
             {
@@ -120,7 +124,7 @@ namespace Biovation.Brands.PW.Manager
             }
 
 
-            Task.Run(async () =>
+            _ = Task.Run(async () =>
             {
                 while (!cancellationToken.IsCancellationRequested)
                 {
