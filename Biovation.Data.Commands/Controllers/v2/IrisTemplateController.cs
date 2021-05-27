06bdcc12cbbde678370032e329e40cf5fb200ef5
ASarhadi <ASarhadi@PT-ASARHADI>
2021-05-27 15:21:40 +0430

    [Queries]: Add IrisTemplate Controller

---
+++ Biovation.Data.Commands/Controllers/v2/IrisTemplateController.cs
@@ -0,0 +1,34 @@
+﻿using Biovation.Domain;
+using Biovation.Repository.Sql.v2;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace Biovation.Data.Commands.Controllers.v2
+{
+    [ApiController]
+    [Route("biovation/api/v2/[controller]")]
+    public class IrisTemplateController : ControllerBase
+    {
+        private readonly IrisTemplateRepository _irisTemplateRepository;
+
+        public IrisTemplateController(IrisTemplateRepository irisTemplateRepository)
+        {
+            _irisTemplateRepository = irisTemplateRepository;
+        }
+
+        [HttpPut]
+        [Authorize]
+        public Task<ResultViewModel> ModifyIrisTemplate([FromBody] IrisTemplate irisTemplate)
+        {
+            return Task.Run(() => _irisTemplateRepository.ModifyIrisTemplate(irisTemplate));
+        }
+
+        [HttpDelete]
+        [Authorize]
+        public Task<ResultViewModel> DeleteIrisTemplate(long userId = 0, int index = 0)
+        {
+            return Task.Run(() => _irisTemplateRepository.DeleteIrisTemplate(userId, index));
+        }
+    }
+}
