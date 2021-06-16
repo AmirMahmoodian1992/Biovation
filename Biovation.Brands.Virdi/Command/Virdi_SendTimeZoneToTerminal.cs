1da427d6105b462e5ec1f121936943cc064d6836
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-06-16 14:15:59 +0430

    [Virdi]: Handle and fix send timezone to device

--- Biovation.Brands.Virdi/Command/Virdi_SendTimeZoneToTerminal.cs
+++ Biovation.Brands.Virdi/Command/Virdi_SendTimeZoneToTerminal.cs
@@ -2,7 +2,7 @@
 using Biovation.CommonClasses.Interface;
 using Biovation.Constants;
 using Biovation.Domain;
-using Biovation.Service.Api.v1;
+using Biovation.Service.Api.v2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,14 +21,15 @@ namespace Biovation.Brands.Virdi.Command
         private TimeZone TimeZoneObj { get; }
 
         private readonly VirdiServer _virdiServer;
+        private readonly AccessGroupService _accessGroupService;
 
-        public VirdiSendTimeZoneToTerminal(uint code, int timeZoneId, VirdiServer virdiServer, TimeZoneService timeZoneService)
+        public VirdiSendTimeZoneToTerminal(uint code, int timeZoneId, VirdiServer virdiServer, TimeZoneService timeZoneService, AccessGroupService accessGroupService)
         {
             Code = code;
-
             TimeZoneId = timeZoneId;
             _virdiServer = virdiServer;
-            TimeZoneObj = timeZoneService.TimeZones(timeZoneId);
+            _accessGroupService = accessGroupService;
+            TimeZoneObj = timeZoneService.TimeZones(timeZoneId).GetAwaiter().GetResult()?.Data;
             OnlineDevices = virdiServer.GetOnlineDevices();
         }
 
@@ -44,22 +45,10 @@ namespace Biovation.Brands.Virdi.Command
             {
                 Logger.Log($"Time Zone {TimeZoneId} does not exist.");
                 return new ResultViewModel { Code = Convert.ToInt64(TaskStatuses.FailedCode), Id = Code, Message = $"Time Zone {TimeZoneId} does not exist." };
-
             }
 
             try
             {
-                _virdiServer.AccessControlData.InitData();
-
-                foreach (var timeZoneDetail in TimeZoneObj.Details)
-                {
-                    _virdiServer.AccessControlData.SetTimeZone(timeZoneDetail.Id.ToString("D4"), timeZoneDetail.DayNumber,
-                                timeZoneDetail.FromTime.Hours, timeZoneDetail.FromTime.Minutes, timeZoneDetail.ToTime.Hours, timeZoneDetail.ToTime.Minutes);
-                }
-
-                //_callbacks.AccessControlData.SetAuthProperty("0001", 1, 1, 0, 0, 1, 0, 0, 0);
-                //_callbacks.AccessControlData.SetTimeZoneToAuthProperty("0001", 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-                //_callbackInstance.AccessControlData.SetHoliday("1000", 0, 1, 1);
                 _virdiServer.AccessControlData.SetAccessTime(TimeZoneObj.Id.ToString("D4"),
                     TimeZoneObj.Details.FirstOrDefault(tz => tz.DayNumber == 1)?.Id.ToString("D4"),
                     TimeZoneObj.Details.FirstOrDefault(tz => tz.DayNumber == 2)?.Id.ToString("D4"),
@@ -69,14 +58,35 @@ namespace Biovation.Brands.Virdi.Command
                     TimeZoneObj.Details.FirstOrDefault(tz => tz.DayNumber == 6)?.Id.ToString("D4"),
                     TimeZoneObj.Details.FirstOrDefault(tz => tz.DayNumber == 0)?.Id.ToString("D4"), "", "");
 
-                _virdiServer.AccessControlData.SetAccessGroup(TimeZoneObj.Id.ToString("D4"), 0, TimeZoneObj.Id.ToString("D4"));
-
                 // 0: TimeZone information
                 _virdiServer.AccessControlData.SetAccessControlDataToTerminal(0, (int)Code, 0);
-                // 1: Holiday information
-                //_callbacks.AccessControlData.SetAccessControlDataToTerminal(0, terminalID, 1);
-                // 2: AccessTime information
-                _virdiServer.AccessControlData.SetAccessControlDataToTerminal(0, (int)Code, 2);
+
+                var accessGroups = _accessGroupService.GetAccessGroups(timeZoneId: TimeZoneId).GetAwaiter().GetResult()?.Data?.Data;
+                if (accessGroups != null)
+                    foreach (var accessGroup in accessGroups)
+                    {
+                        _virdiServer.AccessControlData.InitData();
+
+                        foreach (var timeZoneDetail in TimeZoneObj.Details)
+                        {
+                            _virdiServer.AccessControlData.SetTimeZone(timeZoneDetail.Id.ToString("D4"),
+                                timeZoneDetail.DayNumber,
+                                timeZoneDetail.FromTime.Hours, timeZoneDetail.FromTime.Minutes,
+                                timeZoneDetail.ToTime.Hours, timeZoneDetail.ToTime.Minutes);
+                        }
+
+                        //_callbacks.AccessControlData.SetAuthProperty("0001", 1, 1, 0, 0, 1, 0, 0, 0);
+                        //_callbacks.AccessControlData.SetTimeZoneToAuthProperty("0001", 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                        //_callbackInstance.AccessControlData.SetHoliday("1000", 0, 1, 1);
+
+                        _virdiServer.AccessControlData.SetAccessGroup(accessGroup.Id.ToString("D4"), 0,
+                            TimeZoneObj.Id.ToString("D4"));
+
+                        // 1: Holiday information
+                        //_callbacks.AccessControlData.SetAccessControlDataToTerminal(0, terminalID, 1);
+                        // 2: AccessTime information
+                        _virdiServer.AccessControlData.SetAccessControlDataToTerminal(0, (int) Code, 2);
+                    }
             }
             catch (Exception exception)
             {
