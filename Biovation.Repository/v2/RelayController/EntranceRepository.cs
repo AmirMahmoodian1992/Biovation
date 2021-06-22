62b23d7f864decad4b76e2cb571752b2ef9b11de
ASarhadi <ASarhadi@PT-ASARHADI>
2021-06-22 15:09:19 +0430

    [Queries]: Fix Repository

--- Biovation.Repository/v2/RelayController/EntranceRepository.cs
+++ Biovation.Repository/v2/RelayController/EntranceRepository.cs
@@ -26,15 +26,15 @@ namespace Biovation.Repository.Sql.v2.RelayController
             {
                 new SqlParameter(nameof(entrance.Name), SqlDbType.NVarChar) {Value = entrance.Name},
                 new SqlParameter(nameof(entrance.Code), SqlDbType.Int) {Value = entrance.Code},
-                new SqlParameter(nameof(entrance.Cameras), SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Cameras) },
-                new SqlParameter(nameof(entrance.Schedulings), SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Schedulings) },
+                new SqlParameter(nameof(entrance.Cameras) + "Json", SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Cameras) },
+                new SqlParameter(nameof(entrance.Schedulings) + "Json", SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Schedulings) },
                 new SqlParameter(nameof(entrance.Description), SqlDbType.NVarChar) {Value = entrance.Description}
             };
 
             return _repository.ToResultList<ResultViewModel>(MethodBase.GetCurrentMethod()?.Name, parameters).Data.FirstOrDefault();
         }
 
-        public ResultViewModel<PagingResult<Entrance>> SelectEntrance(int deviceId, int schedulingId, int id = 0, int code = 0,
+        public ResultViewModel<PagingResult<Entrance>> SelectEntrance(int cameraId, int schedulingId, int id = 0, int code = 0,
             string name = null, string description = null, int pageNumber = 0,
             int pageSize = 0, int nestingDepthLevel = 4)
         {
@@ -43,7 +43,7 @@ namespace Biovation.Repository.Sql.v2.RelayController
                 new SqlParameter(nameof(id), SqlDbType.Int) {Value = id },
                 new SqlParameter(nameof(code), SqlDbType.Int) {Value = code },
                 new SqlParameter(nameof(name), SqlDbType.NVarChar) {Value = name??string.Empty},
-                new SqlParameter(nameof(deviceId), SqlDbType.Int) { Value = deviceId },
+                new SqlParameter(nameof(cameraId), SqlDbType.Int) { Value = cameraId },
                 new SqlParameter(nameof(schedulingId), SqlDbType.Int) { Value = schedulingId },
                 new SqlParameter(nameof(pageNumber), SqlDbType.Int) {Value = pageNumber},
                 new SqlParameter(nameof(pageSize), SqlDbType.Int) {Value = pageSize}
@@ -56,10 +56,11 @@ namespace Biovation.Repository.Sql.v2.RelayController
         {
             var parameters = new List<SqlParameter>
             {
+                new SqlParameter(nameof(entrance.Id), SqlDbType.Int) {Value = entrance.Id},
                 new SqlParameter(nameof(entrance.Name), SqlDbType.NVarChar) {Value = entrance.Name},
-                new SqlParameter(nameof(entrance.Code), SqlDbType.Int) {Value = entrance.Code},
-                new SqlParameter(nameof(entrance.Cameras), SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Cameras) },
-                new SqlParameter(nameof(entrance.Schedulings), SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Schedulings) },
+                new SqlParameter(nameof(entrance.Code), SqlDbType.BigInt) {Value = entrance.Code},
+                new SqlParameter(nameof(entrance.Cameras) + "Json", SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Cameras) },
+                new SqlParameter(nameof(entrance.Schedulings) + "Json", SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(entrance.Schedulings) },
                 new SqlParameter(nameof(entrance.Description), SqlDbType.NVarChar) {Value = entrance.Description}
             };
 
