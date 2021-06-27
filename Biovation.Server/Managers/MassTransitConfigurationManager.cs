a2d217a1a160ccf58414b682fed0edc94c39491f
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-06-27 17:05:30 +0430

    [Server]: Add mass transit libraries

---
+++ Biovation.Server/Managers/MassTransitConfigurationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Biovation.Server.Managers
+{
+    public static class MassTransitConfigurationManager
+    {
+        public static void ConfigureMassTransit(this IServiceCollection services)
+        {
+
+        }
+    }
+}
