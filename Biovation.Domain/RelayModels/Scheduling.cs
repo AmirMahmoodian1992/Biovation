18db9f66f4bae45361ad518b02b16d86af80ae63
ASarhadi <ASarhadi@PT-ASARHADI>
2021-06-27 15:42:36 +0430

    [Scheduling]: Fix Enum

--- Biovation.Domain/RelayModels/Scheduling.cs
+++ Biovation.Domain/RelayModels/Scheduling.cs
@@ -5,10 +5,15 @@ namespace Biovation.Domain.RelayModels
 {
     public class Scheduling
     {
-        [Id]
-        public int Id { get; set; }
+
+        [Id] public int Id { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
-        public Enum Mode { get; set; }
+
+        public enum Mode
+        {
+            Open,
+            Close
+        };
     }
 }
