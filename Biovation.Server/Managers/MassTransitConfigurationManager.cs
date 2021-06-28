aefac51b3569420098ce828e5617debb5157383d
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-06-28 18:34:30 +0430

    [Server]: Configuring mass transit

--- Biovation.Server/Managers/MassTransitConfigurationManager.cs
+++ Biovation.Server/Managers/MassTransitConfigurationManager.cs
@@ -1,13 +1,15 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
+using System.Linq;
 
 namespace Biovation.Server.Managers
 {
     public static class MassTransitConfigurationManager
     {
-        public static void ConfigureMassTransit(this IServiceCollection services)
+        public static void ConfigureMassTransit(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<HealthCheckPublisherOptions>(options =>
             {
@@ -15,9 +17,26 @@ namespace Biovation.Server.Managers
                 options.Predicate = check => check.Tags.Contains("ready");
             });
 
+            var consumers = typeof(MassTransitConfigurationManager).Assembly.GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsGenericTypeDefinition)
+                .Where(t => typeof(IConsumer).IsAssignableFrom(t) || t.Name.EndsWith("Consumer", StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
+
+
             services.AddMassTransit(config =>
             {
-                config.UsingRabbitMq();
+                config.AddConsumers(consumers.ToArray());
+                config.UsingRabbitMq((context, cfg) =>
+                {
+                    foreach (var consumer in consumers)
+                    {
+                        cfg.ReceiveEndpoint(configuration.GetValue<string>("MassTransit:RabbitMQ:ReceiveEndpoints:Consumer:queueName"), e =>
+                       {
+                           e.Bind(configuration.GetValue<string>("MassTransit:RabbitMQ:ReceiveEndpoints:Consumer:exchangeName"));
+                           e.ConfigureConsumer(context, consumer);
+                       });
+                    }
+                });
             });
 
             services.AddMassTransitHostedService();
