8b82c3cb68ebb077a184451fa6a3f697bac22265
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-06-28 08:11:04 +0430

    [Relay]: Add open/close API

--- Biovation.Services.RelayController/Controllers/RelayController.cs
+++ Biovation.Services.RelayController/Controllers/RelayController.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Linq;
-using Biovation.CommonClasses;
-using Biovation.Constants;
+﻿using Biovation.Constants;
 using Biovation.Domain;
-using Microsoft.AspNetCore.Mvc;
 using Biovation.Services.RelayController.Commands;
 using Biovation.Services.RelayController.Services;
-using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using System;
+using System.Linq;
 using CommandType = Biovation.Services.RelayController.Domain.CommandType;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Biovation.Services.RelayController.Controllers
 {
     [Route("biovation/api/[controller]")]
-
     [ApiController]
     public class RelayController : ControllerBase
     {
@@ -31,28 +26,66 @@ namespace Biovation.Services.RelayController.Controllers
             _logger = logger.ForContext<RelayController>();
         }
 
+        [Route("{relayId:int}/Open")]
+        [HttpPost]
+        public ResultViewModel Open([FromRoute] int relayId, string messagePriority = "13003")
+        {
+            try
+            {
+                var priority = _lookups.TaskPriorities.FirstOrDefault(tp =>
+                    string.Equals(tp.Code, messagePriority, StringComparison.InvariantCultureIgnoreCase));
+
+                var command = _commandFactory.Factory(CommandType.TurnOn, relayId);
+                var result = command.Data.Execute(priority);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new ResultViewModel { Message = e.Message, Code = 500 };
+            }
+        }
+
+        [Route("{relayId:int}/Close")]
+        [HttpPost]
+        public ResultViewModel Close([FromRoute] int relayId, string messagePriority = "13003")
+        {
+            try
+            {
+                var priority = _lookups.TaskPriorities.FirstOrDefault(tp =>
+                    string.Equals(tp.Code, messagePriority, StringComparison.InvariantCultureIgnoreCase));
+
+                var command = _commandFactory.Factory(CommandType.TurnOff, relayId);
+                var result = command.Data.Execute(priority);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new ResultViewModel { Message = e.Message, Code = 500 };
+            }
+        }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="relayId"></param>
+        /// <param name="messagePriority"></param>
         /// <returns></returns>
         [Route("Contact/{relayId:int}")]
         [HttpPost]
         public ResultViewModel Contact([FromRoute] int relayId, string messagePriority = "13003")
         {
-            
             try
             {
+                _logger.Debug("Requesting relay contacting for relay {relayId} with priority {priorityCode}", relayId, messagePriority);
                 var priority = _lookups.TaskPriorities.FirstOrDefault(tp =>
                     string.Equals(tp.Code, messagePriority, StringComparison.InvariantCultureIgnoreCase));
-                 //_logger.Debug("Log for relay {relayId}", relayId);
                 var command = _commandFactory.Factory(CommandType.Contact, relayId);
                 var result = command.Data.Execute(priority);
                 return result;
             }
             catch (Exception e)
             {
-                return new ResultViewModel{Message = e.Message, Code = 500};
+                return new ResultViewModel { Message = e.Message, Code = 500 };
             }
         }
 
@@ -70,7 +103,7 @@ namespace Biovation.Services.RelayController.Controllers
             {
                 var priority = _lookups.TaskPriorities.FirstOrDefault(tp =>
                     string.Equals(tp.Code, messagePriority, StringComparison.InvariantCultureIgnoreCase));
-                
+
                 var command = _commandFactory.Factory(CommandType.TurnOn, relayId);
                 var result = command.Data.Execute(priority);
                 return result;
