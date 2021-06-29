194f1af06af66b54434a32b53360cbc7b944cca1
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-06-29 15:01:57 +0430

    [Server]: Sample consumer added

---
+++ Biovation.Server/Consumers/LicensePlate/PlateInsertionConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Biovation.Server.Consumers.LicensePlate
+{
+    public class PlateInsertionConsumer : IConsumer<Domain.LicensePlate>
+    {
+        private readonly ILogger<PlateInsertionConsumer> _logger;
+
+        public PlateInsertionConsumer(ILogger<PlateInsertionConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<Domain.LicensePlate> context)
+        {
+            _logger.LogInformation(context.Message.LicensePlateNumber);
+            return Task.CompletedTask;
+        }
+    }
+}
