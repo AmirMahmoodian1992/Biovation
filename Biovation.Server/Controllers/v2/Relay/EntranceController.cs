b2956d6ec59c77649e7f5865429cbec0cd4b9d20
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-07-03 15:55:02 +0430

    [Queries]: Fix entrance SP problem

--- Biovation.Server/Controllers/v2/Relay/EntranceController.cs
+++ Biovation.Server/Controllers/v2/Relay/EntranceController.cs
@@ -24,12 +24,12 @@ namespace Biovation.Server.Controllers.v2.Relay
         [HttpGet]
         [Route("{id:int}")]
         [AllowAnonymous]
-        public Task<ResultViewModel<PagingResult<Entrance>>> Entrances([FromRoute] int id = default, string name = null, string description = null, int pageNumber = 0,
+        public async Task<ResultViewModel<PagingResult<Entrance>>> Entrances([FromRoute] int id = default, string name = null, string description = null, int pageNumber = 0,
         int pageSize = 0, int nestingDepthLevel = 4, int cameraId = default, int deviceId = default, int schedulingId = default)
         {
             var token = (string)HttpContext.Items["Token"];
-            var result = Task.Run(async () => await _entranceService.GetEntrances(id, name, description, pageNumber, pageSize,
-                nestingDepthLevel, cameraId, deviceId, schedulingId, token));
+            var result = await _entranceService.GetEntrances(id, name, description, pageNumber, pageSize,
+                nestingDepthLevel, cameraId, deviceId, schedulingId, token);
             return result;
         }
 
