952ba82bb4b246a13cf3e400a8c127cf1ded3296
Omid Yazdizadeh <omid.yazdizadeh95@gmail.com>
2021-07-10 02:36:39 +0430

    [Server]: Mass transit consumer configuration loaded completly

---
+++ Biovation.Domain/MassTransit/MassTransitExchangeConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Biovation.Domain.MassTransit
+{
+    public class MassTransitExchangeConfig
+    {
+        public string ExchangeName { get; set; }
+        public string ExchangeType { get; set; }
+        public bool? Durable { get; set; }
+        public Dictionary<string, string> Arguments { get; set; }
+    }
+}
